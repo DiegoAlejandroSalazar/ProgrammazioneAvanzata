5b5445a6a5dbb6d46d4ac96e1d9a57199c292dde
DiegoAlejandroSalazar <diegoalejndroflco@gmail.com>
2025-04-22 22:48:00 +0200

    provato a riaggiustare visual studio

--- Source/PJ_Prog_Avanzata/InteractableObjects.cpp
+++ Source/PJ_Prog_Avanzata/InteractableObjects.cpp
@@ -8,6 +8,7 @@ AInteractableObjects::AInteractableObjects()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+    SetActorTickEnabled(true);
 	Tags.Add(FName(TEXT("InteractableTag")));
 
 }
@@ -15,17 +16,17 @@ AInteractableObjects::AInteractableObjects()
 // Called when the game starts or when spawned
 void AInteractableObjects::BeginPlay()
 {
-	
+    if (GEngine)
+        GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Blue, TEXT("Oggetto in movimento"));
 }
-
-// Called every frame
-void AInteractableObjects::Tick(float DeltaTime)
+void AInteractableObjects::CheckIfMoving()
 {
-	Super::Tick(DeltaTime);
-	if (GetVelocity().IsNearlyZero(1.0f)) {
-		
+    FVector CurrentVelocity = GetVelocity(); // Assuming GetVelocity() is available on your actor
+
+    if (CurrentVelocity.IsNearlyZero(1.0f)) // Use a tolerance for comparison
+    {
         if (GEngine)
-            GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Blue, TEXT("Oggetto fermo"));
+            GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Blue, TEXT("Oggetto fermo"));;
         // Perform actions when the actor is not moving
     }
     else
@@ -34,6 +35,15 @@ void AInteractableObjects::Tick(float DeltaTime)
             GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Blue, TEXT("Oggetto in movimento"));
         // Perform actions when the actor is moving
     }
+}
+
+// Called every frame
+void AInteractableObjects::Tick(float DeltaTime)
+{
+    if (GEngine)
+        GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Blue, TEXT("Oggetto in movimento"));
+    Super::Tick(DeltaTime);
+    CheckIfMoving();
 
 }
 
--- Source/PJ_Prog_Avanzata/InteractableObjects.h
+++ Source/PJ_Prog_Avanzata/InteractableObjects.h
@@ -21,5 +21,7 @@ protected:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+	UFUNCTION()
+	void CheckIfMoving();
 
 };
